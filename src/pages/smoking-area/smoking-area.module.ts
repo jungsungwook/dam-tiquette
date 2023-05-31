1aa1550d09d0509e8d8b42c3274d9a70178ca290
jungsungwook <jswcyber@naver.com>
2023-05-31 16:28:19 +0900

    feat: smoking area service

---
+++ src/pages/smoking-area/smoking-area.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { SmokingArea, SmokingAreaComment, SmokingAreaRate } from "./smoking-area.entity";
+import { SmokingAreaController } from "./smoking-area.controller";
+import { SmokingAreaService } from "./smoking-area.service";
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            SmokingArea,
+            SmokingAreaComment,
+            SmokingAreaRate,
+        ]),
+    ],
+    controllers: [SmokingAreaController],
+    providers: [SmokingAreaService]
+})
+export class SmokingAreaModule { }
