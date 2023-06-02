d8f81410b044316530ef5b200946bc40792a98ca
jungsungwook <jswcyber@naver.com>
2023-06-02 16:46:26 +0900

    fix: export service module

--- src/pages/users/users.module.ts
+++ src/pages/users/users.module.ts
@@ -13,5 +13,6 @@ import { User } from './user.entity';
   ],
   controllers: [UsersController],
   providers: [UsersService, UserRepository],
+  exports: [UsersService, TypeOrmModule]
 })
 export class UsersModule {}
