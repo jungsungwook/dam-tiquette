03a964b4b7c1d557c10d663268a0d179b5257fcc
jungsungwook <jswcyber@naver.com>
2023-06-02 16:48:20 +0900

    feat: smoking area crud

--- src/pages/smoking-area/smoking-area.controller.ts
+++ src/pages/smoking-area/smoking-area.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Req, UseInterceptors, HttpException, Param, Query } from "@nestjs/common";
+import { Controller, Get, Req, UseInterceptors, HttpException, Param, Query, Post, Body, UseGuards } from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { SmokingAreaService } from "./smoking-area.service";
 import { TransactionInterceptor } from "src/decorators/TransactionInterceptor.decorator";
 import { TransactionManager } from "src/decorators/TransactionManager.decorator";
 import { EntityManager } from "typeorm";
-import { SmokingAreaDto } from "./dto/smoking-area.dto";
+import { SmokingAreaCommentCreateDto, SmokingAreaCommentDto, SmokingAreaCreateDto, SmokingAreaDetailDto, SmokingAreaDto, SmokingAreaRateDto } from "./dto/smoking-area.dto";
+import { AuthGuard } from "@nestjs/passport";
+import { GetUser } from "src/auth/get-user.decorator";
+import { User } from "../users/user.entity";
 
 
 @Controller('smoking-area')
@@ -27,10 +30,22 @@ export class SmokingAreaController {
         @Query('page') page: number = 1,
     ): Promise<{ statusCode: string, contents: SmokingAreaDto[] }> {
         try {
+            console.log("request url: ", req.url)
+            console.log("receive: search", search)
             // 쿼리 파라미터가 없을 경우
             if (!search && !tags && !address) {
                 const result = await this.smokingAreaService.getAllSmokingArea(
                     queryRunnerManager,
+                    limit,
+                    page
+                );
+                return { statusCode: '200', contents: result };
+            } else {
+                const result = await this.smokingAreaService.getSmokingAreaBySearch(
+                    queryRunnerManager,
+                    search,
+                    tags,
+                    address,
                     limit,
                     page
                 );
@@ -48,11 +63,123 @@ export class SmokingAreaController {
         @Req() req: Request,
         @TransactionManager() queryRunnerManager: EntityManager,
         @Param('id') id: number
-    ): Promise<{ statusCode: string, contents: SmokingAreaDto }> {
+    ): Promise<{ statusCode: string, contents: SmokingAreaDetailDto }> {
         try {
             const result = await this.smokingAreaService.getSmokingAreaDetail(
                 queryRunnerManager,
                 id
+            );
+            return { statusCode: '200', contents: result };
+        } catch (e) {
+            throw new HttpException(e.message, e.status | 500);
+        }
+    }
+
+    @Post('/')
+    @UseGuards(AuthGuard('jwt'))
+    @ApiOperation({ summary: '흡연 구역 등록(신청)' })
+    async createSmokingArea(
+        @Req() req: Request,
+        @TransactionManager() queryRunnerManager: EntityManager,
+        @Body() body: SmokingAreaCreateDto,
+        @GetUser() user: User,
+    ): Promise<{ statusCode: string, contents: SmokingAreaDto }> {
+        try {
+            const result = await this.smokingAreaService.createSmokingArea(
+                queryRunnerManager,
+                body,
+                user
+            );
+            return { statusCode: '200', contents: result };
+        } catch (e) {
+            throw new HttpException(e.message, e.status | 500);
+        }
+    }
+
+    @Get('/:id/comment')
+    @ApiOperation({ summary: '흡연 구역 댓글 조회' })
+    async getSmokingAreaComment(
+        @Req() req: Request,
+        @TransactionManager() queryRunnerManager: EntityManager,
+        @Param('id') id: number,
+        @Query('limit') limit: number = 10,
+        @Query('page') page: number = 1,
+    ): Promise<{ statusCode: string, contents: SmokingAreaCommentDto[] }> {
+        try {
+            const result = await this.smokingAreaService.getSmokingAreaComment(
+                queryRunnerManager,
+                id,
+                limit,
+                page
+            );
+            return { statusCode: '200', contents: result };
+        } catch (e) {
+            throw new HttpException(e.message, e.status | 500);
+        }
+    }
+
+    @Post('/:id/comment')
+    @UseGuards(AuthGuard('jwt'))
+    @ApiOperation({ summary: '흡연 구역 댓글 등록' })
+    async createSmokingAreaComment(
+        @Req() req: Request,
+        @TransactionManager() queryRunnerManager: EntityManager,
+        @Param('id') id: number,
+        @Body() body: SmokingAreaCommentCreateDto,
+        @GetUser() user: User,
+    ): Promise<{ statusCode: string, contents: SmokingAreaCommentDto }> {
+        try {
+            const result = await this.smokingAreaService.createSmokingAreaComment(
+                queryRunnerManager,
+                id,
+                body,
+                user
+            );
+            return { statusCode: '200', contents: result };
+        } catch (e) {
+            throw new HttpException(e.message, e.status | 500);
+        }
+    }
+
+    @Get('/:id/rate')
+    @ApiOperation({ summary: '흡연 구역 평점 조회' })
+    async getSmokingAreaRate(
+        @Req() req: Request,
+        @TransactionManager() queryRunnerManager: EntityManager,
+        @Param('id') id: number,
+    ): Promise<{
+        statusCode: string, contents: {
+            rating: number,
+            ratingCount: number,
+        }
+    }> {
+        try {
+            const rating = await this.smokingAreaService.getSmokingAreaRating(
+                queryRunnerManager,
+                id,
+            );
+            return { statusCode: '200', contents: rating };
+        } catch (e) {
+            throw new HttpException(e.message, e.status | 500);
+        }
+    }
+
+    @Post('/:id/rate')
+    @UseGuards(AuthGuard('jwt'))
+    @ApiOperation({ summary: '흡연 구역 평점 등록' })
+    async createSmokingAreaRate(
+        @Req() req: Request,
+        @TransactionManager() queryRunnerManager: EntityManager,
+        @Param('id') id: number,
+        @Body() body: { rate: number },
+        @GetUser() user: User,
+    ): Promise<{ statusCode: string, contents: SmokingAreaRateDto }> {
+        try {
+            const result = await this.smokingAreaService.createSmokingAreaRate(
+                queryRunnerManager,
+                id,
+                body.rate,
+                user
             );
             return { statusCode: '200', contents: result };
         } catch (e) {
--- src/pages/smoking-area/smoking-area.service.ts
+++ src/pages/smoking-area/smoking-area.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { EntityManager } from 'typeorm';
-import { SmokingArea } from './smoking-area.entity';
-import { SmokingAreaDto } from './dto/smoking-area.dto';
+import { SmokingArea, SmokingAreaComment, SmokingAreaRate } from './smoking-area.entity';
+import { SmokingAreaCommentCreateDto, SmokingAreaCommentDto, SmokingAreaCreateDto, SmokingAreaDetailDto, SmokingAreaDto, SmokingAreaRateDto } from './dto/smoking-area.dto';
+import { User } from '../users/user.entity';
 @Injectable()
 export class SmokingAreaService {
     constructor(
@@ -15,7 +16,8 @@ export class SmokingAreaService {
         try {
             const smokingAreas = await queryRunnerManager.find(SmokingArea, {
                 where: {
-                    isDeleted: false
+                    isDeleted: false,
+                    isApproved: true
                 },
                 take: limit,
                 skip: (page - 1) * limit
@@ -26,15 +28,196 @@ export class SmokingAreaService {
         }
     }
 
-    async getSmokingAreaDetail(queryRunnerManager: EntityManager, id: number): Promise<SmokingAreaDto> {
+    async getSmokingAreaBySearch(
+        queryRunnerManager: EntityManager,
+        search?: string,
+        tags?: string,
+        address?: string,
+        limit: number = 10,
+        page: number = 1,
+    ): Promise<SmokingAreaDto[]> {
+        try {
+            const smokingAreas = await queryRunnerManager.find(SmokingArea, {
+                where: {
+                    isDeleted: false,
+                    isApproved: true
+                },
+                take: limit,
+                skip: (page - 1) * limit
+            }).then((result) => {
+                return result.filter((item) => {
+                    if (search) {
+                        if (item.name && item.name.includes(search)) {
+                            return true;
+                        } else if (item.address && item.address.includes(search)) {
+                            return true;
+                        } else if (item.description && item.description.includes(search)) {
+                            return true;
+                        } else if (item.tags && item.tags.includes(search)) {
+                            return true;
+                        }
+                    }
+                    if (tags) {
+                        if (item.tags && item.tags.includes(tags)) {
+                            return true;
+                        }
+                    }
+                    if (address) {
+                        if (item.address && item.address.includes(address)) {
+                            return true;
+                        }
+                    }
+                    return false;
+                })
+            });
+            return smokingAreas;
+        } catch (e) {
+            console.log(e)
+            throw e;
+        }
+    }
+
+    async getSmokingAreaDetail(queryRunnerManager: EntityManager, id: number): Promise<SmokingAreaDetailDto> {
         try {
             const smokingArea = await queryRunnerManager.findOne(SmokingArea, {
                 where: {
                     id: id,
-                    isDeleted: false
+                    isDeleted: false,
+                    isApproved: true
                 }
+            });
+            const smokingAreaComments = await this.getSmokingAreaComment(queryRunnerManager, id, 10, 1);
+            const smokingAreaRates = await this.getSmokingAreaRates(queryRunnerManager, id);
+            const smokingAreaDetail: SmokingAreaDetailDto = {
+                smokingArea: smokingArea,
+                rating: smokingAreaRates.reduce((acc, cur) => acc + cur.rate, 0) / smokingAreaRates.length,
+                ratingCount: smokingAreaRates.length,
+                comments: smokingAreaComments
+            }
+            return smokingAreaDetail;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async createSmokingArea(
+        queryRunnerManager: EntityManager,
+        smokingAreaDto: SmokingAreaCreateDto,
+        user: User
+    ): Promise<SmokingAreaDto> {
+        try {
+            const smokingArea: SmokingAreaDto = await queryRunnerManager.save(SmokingArea, {
+                ...smokingAreaDto,
+                createdBy: user.customId,
+                updatedBy: user.customId
             })
             return smokingArea;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async getSmokingAreaComment(
+        queryRunnerManager: EntityManager,
+        smokingAreaId: number,
+        limit: number,
+        page: number,
+    ): Promise<SmokingAreaCommentDto[]> {
+        try {
+            const smokingAreaComments = await queryRunnerManager.find(SmokingAreaComment, {
+                where: {
+                    smokingAreaId: smokingAreaId,
+                    isDeleted: false
+                },
+                take: limit,
+                skip: (page - 1) * limit
+            });
+            return smokingAreaComments;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async createSmokingAreaComment(
+        queryRunnerManager: EntityManager,
+        smokingAreaId: number,
+        comment: SmokingAreaCommentCreateDto,
+        user: User
+    ): Promise<SmokingAreaCommentDto> {
+        try {
+            const smokingAreaComment: SmokingAreaComment = await queryRunnerManager.save(SmokingAreaComment, {
+                smokingAreaId: smokingAreaId,
+                comment: comment,
+                createdBy: user.customId,
+                updatedBy: user.customId
+            });
+            return smokingAreaComment;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async getSmokingAreaRates(
+        queryRunnerManager: EntityManager,
+        smokingAreaId: number,
+    ): Promise<SmokingAreaRateDto[]> {
+        try {
+            const smokingAreaRate: SmokingAreaRateDto[] = await queryRunnerManager.find(SmokingAreaRate, {
+                where: {
+                    smokingAreaId: smokingAreaId,
+                    isDeleted: false
+                }
+            });
+            return smokingAreaRate;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async getSmokingAreaRating(
+        queryRunnerManager: EntityManager,
+        smokingAreaId: number,
+    ): Promise<{ rating: number, ratingCount: number }> {
+        try {
+            const smokingAreaRate: SmokingAreaRateDto[] = await queryRunnerManager.find(SmokingAreaRate, {
+                where: {
+                    smokingAreaId: smokingAreaId,
+                    isDeleted: false
+                }
+            });
+            return {
+                rating: smokingAreaRate.reduce((acc, cur) => acc + cur.rate, 0) / smokingAreaRate.length,
+                ratingCount: smokingAreaRate.length
+            }
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async createSmokingAreaRate(
+        queryRunnerManager: EntityManager,
+        smokingAreaId: number,
+        rate: number,
+        user: User
+    ): Promise<SmokingAreaRateDto> {
+        try {
+            const alreadRate = await queryRunnerManager.findOne(SmokingAreaRate, {
+                where: {
+                    smokingAreaId: smokingAreaId,
+                    createdBy: user.customId,
+                    isDeleted: false
+                }
+            });
+            if (alreadRate) {
+                throw new Error('이미 평가한 흡연구역입니다.');
+            }
+            const smokingAreaRate: SmokingAreaRateDto = await queryRunnerManager.save(SmokingAreaRate, {
+                smokingAreaId: smokingAreaId,
+                rate: rate,
+                createdBy: user.customId,
+                updatedBy: user.customId
+            });
+            return smokingAreaRate;
         } catch (e) {
             throw e;
         }
